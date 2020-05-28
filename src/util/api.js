d5446fcb56ec5b112bd7bd70878fce9ff9e287e2
Larinel <>
2020-05-28 23:00:08 +0300

    [graphql] api

--- src/util/api.js
+++ src/util/api.js
@@ -5,7 +5,7 @@ const endpoint = 'http://localhost:3001';
 const jwt = window.Twitch.ext.viewer.sessionToken;
 
 const client = new GraphQLClient(endpoint, {
-  credentials: 'include',
+  credentials: 'omit',
   mode: 'no-cors',
   headers: {
     authorization: `Bearer ${jwt}`,
