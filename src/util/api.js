d3b2ea0de9b7feae927ae5bc83b556bfb98bec98
Larinel <>
2020-05-28 22:41:57 +0300

    [graphql] Expose graphql client

---
+++ src/util/api.js
@@ -0,0 +1,15 @@
+import { GraphQLClient } from 'graphql-request'
+
+const endpoint = 'http://localhost:3001';
+
+const jwt = window.Twitch.ext.viewer.sessionToken;
+
+const client = new GraphQLClient(endpoint, {
+  credentials: 'include',
+  mode: 'cors',
+  headers: {
+    authorization: `Bearer ${jwt}`,
+  }
+})
+
+export default client;
