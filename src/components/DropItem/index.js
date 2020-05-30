ad88b509e17bbbc7a1353037719b718da6a85015
Larinel <>
2020-05-30 23:58:36 +0300

    ...

--- src/components/DropItem/index.js
+++ src/components/DropItem/index.js
@@ -3,7 +3,7 @@ import { Card, Button, Form } from 'react-bootstrap'
 import './index.css'
 import Authentication from '../../util/Authentication/Authentication';
 
-function DropItem({ name} ) {
+function DropItem({ name: itemName, imageLink} ) {
   const [showKey, setShowKey] = useState(false);
   const [key, setKey] = useState('1234-5678-1011-qqqWq');
 
@@ -19,13 +19,15 @@ function DropItem({ name} ) {
     </Form>
   }
 
+  const [name, descr] = itemName.split('|');
+
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="https://i.pinimg.com/originals/e3/f5/cc/e3f5cc209e26077fba05f841fdf490be.png" />
+      <Card.Img variant="top" src={imageLink} />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Text>
-          Small item explanation on flaw text
+          {descr}
         </Card.Text>
         {showKey ?
           keyForm() :
